b4789db69ba41f5560ace72501e4d9adfdf4d791
Somil Jain <sj.somiljain2006@gmail.com>
2026-03-29 15:45:24 +0530

    new index

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app=application
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
 @app.route("/predictdata",methods=["GET","POST"])
 def predict_datapoints():
     if request.method=="GET":
